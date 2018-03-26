a99eba1c399c03694608e2929007007e53e68a88
glaw1300 <glawrence@thacher.org>
2018-03-26 13:27:29 -0700

    All necessary files for the website. To run, just type, in a terminal, python flask_app.py

---
+++ flask_app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+def connect_db():
+    conn = mysql.connector.connect(user='root', database='astronomy')
+    cur = conn.cursor(buffered=True)
+
+    return conn, cur
+
+def close_connection(cur, conn):
+    cur.close()
+    conn.close()
+
+@app.route('/')
+def splash_page():
+    return render_template("index.html")
+
+@app.route('/image_query')
+def image_query():
+    conn, cur = connect_db()
+
+    close_connection(cur, conn)
+    return render_template("image_query.html")#, headers=headers, data=data)
+
+@app.route('/bodies')
+def bodies():
+    conn, cur = connect_db()
+
+    close_connection(cur, conn)
+    return render_template("bodies.html")#, headers=headers, data=data)
+
+@app.route('/body')
+def body():
+    conn, cur = connect_db()
+
+    close_connection(cur, conn)
+    return render_template("body.html")#, headers=headers, data=data)
+
+@app.route('/night')
+def night():
+    conn, cur = connect_db()
+
+    close_connection(cur, conn)
+    return render_template("night.html")#, headers=headers, data=data)
+
+@app.route('/nights')
+def nights():
+    conn, cur = connect_db()
+
+    close_connection(cur, conn)
+    return render_template("nights.html")#, headers=headers, data=data)
+
+@app.route('/observatory_stats')
+def obs_stats():
+    conn, cur = connect_db()
+
+    close_connection(cur, conn)
+    return render_template("observatory_stats.html")#, headers=headers, data=data)
+
+@app.route('/autocomplete', methods=['GET'])
+def autocomplete():
+    conn, cur = connect_db()
+
+    cur.execute("SELECT name FROM bodies")
+    suggestion = cur.fetchall()
+    map(lambda x: x[0], bodies)
+
+    return jsonify(json_list=suggestion)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/astro.js
@@ -0,0 +1,1 @@
+var search = ["Batman", "Superman", "Robin", "Batwoman", "batwoman"]
